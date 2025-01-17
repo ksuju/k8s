2c4c00e806d9de8d89fd77ad6bd66d346961a05b
ksuju <139477852+ksuju@users.noreply.github.com>
2025-01-17 15:26:11 +0900

    test

--- src/main/java/com/ll/k8s/domain/home/controller/HomeController.java
+++ src/main/java/com/ll/k8s/domain/home/controller/HomeController.java
@@ -29,7 +29,7 @@ public class HomeController {
         InetAddress localHost = InetAddress.getLocalHost();
         // getHostName 메소드를 호출하여 호스트 이름을 얻습니다.
         String hostname = localHost.getHostName();
-        return "home, hostname : " + hostname;
+        return "홈, hostname : " + hostname;
     }
     @GetMapping("/jwtSecretKey")
     @ResponseBody
