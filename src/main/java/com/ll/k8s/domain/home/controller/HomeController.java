17837e80dd0dc1c82351af5f36771811309466bf
ksuju <139477852+ksuju@users.noreply.github.com>
2025-01-17 14:46:50 +0900

    yes!

--- src/main/java/com/ll/k8s/domain/home/controller/HomeController.java
+++ src/main/java/com/ll/k8s/domain/home/controller/HomeController.java
@@ -5,6 +5,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 /**
  * packageName    : com.ll.k8s.domain.home.controller
  * fileName       : HomeController
@@ -22,8 +25,11 @@ public class HomeController {
     private String jwtSecretKey;
     @GetMapping("/")
     @ResponseBody
-    public String home() {
-        return "home";
+    public String showMain() throws UnknownHostException {
+        InetAddress localHost = InetAddress.getLocalHost();
+        // getHostName 메소드를 호출하여 호스트 이름을 얻습니다.
+        String hostname = localHost.getHostName();
+        return "home, hostname : " + hostname;
     }
     @GetMapping("/jwtSecretKey")
     @ResponseBody
